c0741e641aa66dc119a0819f6a2bdfb5a21e4dfb
Haru <sitshannawork@gmail.com>
2023-11-14 20:19:15 -0500

    Adding Bootstrap practice code

--- data-structures/js/simpleQueue.js
+++ data-structures/js/simpleQueue.js
@@ -1,14 +1,68 @@
 class SimpleQueue {
-    /*
-      constructor(elements) {
-    this._elements = Array.isArray(elements) ? elements : [];
-    this._offset = 0;
+  constructor(elements) {
+    this.elements = Array.isArray(elements) ? elements : [];
   }
-  */
 
-    //enqueue (push)
-    //dequeue(pop)
-    // isEmpty
-    // size
-    // clear
+  // Think: Push for Queues
+  enqueue(value) {
+    this.elements.push(value);
+  }
+
+  // Think: Pop for Queues
+  dequeue() {
+    this.elements.splice(0, 1);
+  }
+
+  isEmpty() {
+    return this.elements.length === 0;
+  }
+
+  size() {
+    return this.elements.length;
+  }
+
+  clear() {
+    this.elements = [];
+  }
+
+  printQueue() {
+    if(this.elements.length > 0) {
+      const queueStr = this.elements.join(", ");
+      console.log(queueStr);
+    } else {
+      console.log("Empty Queue");
+    }
+  }
 }
+
+// Note in this stack, the first element is the last element of the array.
+const testExample = new SimpleQueue();
+const testFullStack = new SimpleQueue([1,2,3,4]);
+testExample.printQueue();
+testFullStack.printQueue();
+console.log("\n");
+
+testExample.enqueue(4);
+testExample.enqueue(3);
+testExample.enqueue(2);
+testExample.enqueue(1);
+testExample.printQueue();
+console.log("\n");
+
+testFullStack.dequeue();
+testFullStack.printQueue();
+testFullStack.dequeue();
+testFullStack.printQueue();
+testFullStack.dequeue();
+testFullStack.printQueue();
+testFullStack.dequeue();
+testFullStack.printQueue();
+console.log("\n");
+
+console.log("Is this queue empty?", testFullStack.isEmpty());
+console.log("\n");
+
+console.log("What is the size of the queue? ", testExample.size());
+testExample.clear();
+console.log("Is it empty after I clear it? ", testExample.isEmpty());
+
--- data-structures/js/simpleStack.js
+++ data-structures/js/simpleStack.js
@@ -1,24 +1,76 @@
 class SimpleStack {
   constructor(elements) {
-    this._elements = Array.isArray(elements) ? elements : [];
+    this.elements = Array.isArray(elements) ? elements : [];
   }
 
-  push() {}
+  push(value) {
+    this.elements.push(value);
+  }
 
-  peek() {}
+  peek() {
+    return this.elements.indexOf(this.elements.length - 1);
+  }
 
-  pop() {}
+  pop() {
+    this.elements.pop();
+  }
 
-  isEmpty() {}
+  isEmpty() {
+    return this.elements.length === 0;
+  }
 
-  size() {}
+  size() {
+    return this.elements.length;
+  }
 
-  clear() {}
+  clear() {
+    this.elements = [];
+  }
 
   printStack() {
-    const stackStr = elements.join(", ");
-    console.log(stackStr);
+    if(this.elements.length > 0) {
+      const stackStr = this.elements.join(", ");
+      console.log(stackStr);
+    } else {
+      console.log("Empty Stack")
+    }
   }
 }
 
-const testExample = SimpleStack() {}
+// Note in this stack, the first element is the last element of the array.
+const testExample = new SimpleStack();
+const testFullStack = new SimpleStack([1,2,3,4]);
+testExample.printStack();
+testFullStack.printStack();
+console.log("\n");
+// TestExample should print: Empty Stack
+// TestFullStack should print: 1, 2, 3, 4
+
+testExample.push(4);
+testExample.push(3);
+testExample.push(2);
+testExample.push(1);
+testExample.printStack();
+console.log("If we peek, we should get (1): ", testExample.peek());
+// TestExample should print: 4, 3, 2, 1
+
+testFullStack.pop();
+testFullStack.pop();
+testFullStack.pop();
+testFullStack.pop();
+testFullStack.printStack();
+// TestFullStack should print: Empty Stack
+console.log("Is this stack empty?", testFullStack.isEmpty());
+console.log("\n");
+
+console.log("What is the size of the stack? ", testExample.size());
+testExample.clear();
+console.log("Is it empty after I clear it? ", testExample.isEmpty());
+
+
+
+
+
+
+
+
