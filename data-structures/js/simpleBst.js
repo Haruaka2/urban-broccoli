d58da301f3db11cd8f8920ec5a7927ca426e110c
Haru <sitshannawork@gmail.com>
2023-11-20 20:06:37 -0500

    Continue practicing coding

--- data-structures/js/simpleBst.js
+++ data-structures/js/simpleBst.js
@@ -13,49 +13,79 @@ class SimpleBst {
 
   recursiveInsert(node, newNode) {
     //right
-    if(newNode.data >= node) {
-
+    if(newNode.data >= node.data) {
+      if(node.right) {
+        this.recursiveInsert(node.right, newNode);
+      } else {
+        node.right = newNode;
+      }
     } else {
-      // left
       if(node.left) {
-        // recusrive call;
+        this.recursiveInsert(node.left, newNode);
       } else {
         node.left = newNode;
       }
     }
   }
-  
   insert(value) {
     const newNode = new BstNode(value, null, null);
     if(this.root) {
-        
+        this.recursiveInsert(this.root, newNode);
     } else {
       this.root = newNode;
     }
   }
-  remove() {}
+
+  recursiveRemove(value, node) {
+    if(this.root.data === value) {
+      
+    }
+
+  }
+  remove(value) {
+    if(this.root) {
+      this.recursiveRemove(value, this.root);
+    } else {
+      return;
+    }
+  }
+  traverseInOrder () {}
+  traversePreOrder () {}
+  traversePostOrder () {}
   find() {}
   min() {}
   max() {}
   root() {
     return this.root;
   }
-  traverseInOrder () {}
-  traversePreOrder () {}
-  traversePostOrder () {}
-  clear() {}
 
+  clear() {}
   recursivePrint(value, bstNode, isLeft) {
     if(bstNode != null) {
       console.log(value + (isLeft ? "|-- " : "\\-- ") + bstNode.data);
-      print(value + (isLeft ? "|   " : "    "), bstNode.left, true);
-      print(value + (isLeft ? "|   " : "    "), bstNode.right, false);
+      this.recursivePrint(value + (isLeft ? "|   " : "    "), bstNode.left, true);
+      this.recursivePrint(value + (isLeft ? "|   " : "    "), bstNode.right, false);
     }
   }
   printBst() {
-    this.recursivePrint("", root, false);
+    if(this.root) {
+      this.recursivePrint("", this.root, false);
+    } else {
+      console.log("Empty Binary Search Tree");
+    }
   }
 }
 
 const exampleTest = new SimpleBst();
+exampleTest.printBst();
 
+exampleTest.insert(10);
+exampleTest.insert(5);
+exampleTest.insert(15);
+exampleTest.insert(3);
+exampleTest.insert(8);
+exampleTest.insert(12);
+exampleTest.insert(25);
+exampleTest.insert(1);
+exampleTest.insert(4);
+exampleTest.printBst();
